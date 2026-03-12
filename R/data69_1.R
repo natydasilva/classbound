894919ba014fea6201d58b0f02bb43dff9fc2ad4
natydasilva <natalia.dasilva@fcea.edu.uy>
2026-03-12 13:49:26 -0300

    include data

---
+++ R/data69_1.R
@@ -0,0 +1,21 @@
+#' data69-1
+#' 
+#' @format  There are 5000 observations, 21 features and 3 classes
+#'
+#' \describe{
+#' \item{Y}{} 
+#' \item{V1}{}
+#' \item{V2}{}
+#' \item{V3}{}
+#' \item{V4}{}
+#' \item{V5}{}
+#' \item{V6}{}
+#' \item{...}{}
+#' }
+#' @docType data
+#' @keywords datasets
+#' @name data69_1
+#' @usage data(data69_1)
+#' @format A data frame with 5000 rows and 21 features and tree classes
+#' @source <https://archive.ics.uci.edu/dataset/107/waveform+database+generator+version+1>
+NULL
